9759811d7a73cecab855c7fe1adde3af717a447f
naul2354 <conco2354@gmail.com>
2022-10-26 09:40:49 +0700

    Add project jhipster on git

---
+++ src/main/java/org/springbootjhipster/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package org.springbootjhipster.repository;
+
+import org.springbootjhipster.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
